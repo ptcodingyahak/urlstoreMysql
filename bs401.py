f2729a158096caa6a612ed45c14e29400981c4c0
ptcodingyahak <codingyahakpt@gmail.com>
2019-02-08 14:38:33 +0900

    webpage data changed html with parsing.

--- bs401.py
+++ bs401.py
@@ -1,7 +1,10 @@
 # Fetching URLs
 import urllib.request
+from bs4 import BeautifulSoup
 
 def fetching01():
     response = urllib.request.urlopen('https://ko.wikipedia.org/wiki/%EC%BB%B4%ED%93%A8%ED%84%B0_%EA%B3%BC%ED%95%99')
-    html = response.read()
-    return html
+    response = BeautifulSoup(response, 'html.parser')
+    return response
+
+# return web page source changed html parser
